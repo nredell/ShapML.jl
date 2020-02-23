9cb2798a9fd7c3a10b39fd3ccb74693a4c0ee647
Nick Redell <nickredell@hotmail.com>
2020-02-22 18:27:23 -0600

    Add unit tests for parallel computation

--- test/shap.jl
+++ test/shap.jl
@@ -25,7 +25,6 @@ using Distributed
       return data_pred
     end
 
-    Random.seed!(224)
     data_shap = ShapML.shap(explain = data[:, 2:end],
                             model = model_mean,
                             predict_function = predict_function,
@@ -37,7 +36,6 @@ using Distributed
     @test size(data_shap, 1) == (size(data, 1) * (size(data, 2) - 1))
     #--------------------------------------------------------------------------
     # Test that the Shapley values are the same for a subset of target features.
-    Random.seed!(224)
     data_shap_feature = ShapML.shap(explain = data[:, 2:end],
                                     model = model_mean,
                                     predict_function = predict_function,
@@ -48,4 +46,51 @@ using Distributed
     @test all(data_shap[data_shap.feature_name .== "x2", [:shap_effect]].shap_effect .== data_shap_feature[data_shap_feature.feature_name .== "x2", [:shap_effect]].shap_effect)
     #--------------------------------------------------------------------------
 end
+#------------------------------------------------------------------------------
+
+@testset "parallel and non-parallel are the same." begin
+
+    addprocs(2)
+
+    @everywhere begin
+        using ShapML
+        using DataFrames
+    end
+
+    data = DataFrame(y = 1:10, x1 = 1:10, x2 = 1:10, x3 = 1:10)
+
+    @everywhere function model_mean(data)
+        y_pred = Array{Float64}(undef, size(data, 1))
+        for i in 1:size(data, 1)
+            y_pred[i] = mean(data[i, 2:end])
+        end
+        return DataFrame(y_pred = y_pred)
+    end
+
+    @everywhere function predict_function(model, data)
+      data_pred = model(data)
+      return data_pred
+    end
+
+    data_shap = ShapML.shap(explain = data[:, 2:end],
+                            model = model_mean,
+                            predict_function = predict_function,
+                            sample_size = 10,
+                            parallel = :none,
+                            seed = 1
+                            )
+
+    data_shap_parallel = ShapML.shap(explain = data[:, 2:end],
+                                     model = model_mean,
+                                     predict_function = predict_function,
+                                     sample_size = 10,
+                                     parallel = :samples,
+                                     seed = 1
+                                     )
+    #--------------------------------------------------------------------------
+    # Test that each instance has a Shapley value for each feature.
+    @test data_shap == data_shap_parallel
+    #--------------------------------------------------------------------------
+end
+
 end  # End test module.
