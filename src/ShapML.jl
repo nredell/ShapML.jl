bc2b036f56a36e115a52638cbfa8482c7b68b4f5
Nick Redell <nickredell@hotmail.com>
2020-03-08 20:35:49 -0500

    Experimental - custom group by for speed on large datasets

--- src/ShapML.jl
+++ src/ShapML.jl
@@ -1,5 +1,60 @@
 module ShapML
 
+# using RDatasets
+# using DataFrames
+# using MLJ  # Machine learning
+#using Gadfly  # Plotting
+#
+# # Load data.
+# boston = RDatasets.dataset("MASS", "Boston")
+# #------------------------------------------------------------------------------
+# # Train a machine learning model; currently limited to single outcome regression and binary classification.
+# outcome_name = "MedV"
+#
+# # Data prep.
+# y, X = MLJ.unpack(boston, ==(Symbol(outcome_name)), colname -> true)
+#
+# # Instantiate an ML model; choose any single-outcome ML model from any package.
+# random_forest = @load RandomForestRegressor pkg = "DecisionTree"
+# model = MLJ.machine(random_forest, X, y)
+#
+# # Train the model.
+# using Random
+# Random.seed!(1)
+# fit!(model)
+#
+# # Create a wrapper function that takes the following positional arguments: (1) a
+# # trained ML model from any Julia package, (2) a DataFrame of model features. The
+# # function should return a 1-column DataFrame of predictions--column names do not matter.
+# function predict_function(model, data)
+#   data_pred = DataFrame(y_pred = MLJ.predict(model, data))
+#   return data_pred
+# end
+# #------------------------------------------------------------------------------
+# # ShapML setup.
+# explain = copy(boston) # Compute Shapley feature-level predictions for 300 instances.
+# explain = select(explain, Not(Symbol(outcome_name)))  # Remove the outcome column.
+#
+# reference = copy(boston)  # An optional reference population to compute the baseline prediction.
+# reference = select(reference, Not(Symbol(outcome_name)))
+#
+# sample_size = 60  # Number of Monte Carlo samples.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 using Distributed
 using DataFrames
 using Random
@@ -43,6 +98,10 @@ Compute stochastic feature-level Shapley values for any ML model.
     + `shap_effect_sd`: The standard deviation of Shapley values across Monte Carlo samples.
     + `intercept`: The average model prediction from `explain` or `reference`.
 """
+# target_features = nothing
+# parallel = nothing
+# seed = 1
+# precision = nothing
 function shap(;explain::DataFrame,
               reference::Union{DataFrame, Nothing} = nothing,
               model,
@@ -155,6 +214,9 @@ function shap(;explain::DataFrame,
                          model = model,  # input arg.
                          predict_function = predict_function,  # input arg.
                          n_features = n_features,  # Calculated.
+                         n_target_features = n_target_features,  # Calculated.
+                         n_instances_explain = n_instances_explain,  # Calculated.
+                         sample_size = sample_size,  # input arg.
                          precision = precision  # input arg.
                          )
     #--------------------------------------------------------------------------
--- src/predict.jl
+++ src/predict.jl
@@ -1,7 +1,8 @@
 # Internal prediction function. Used at the end of shap().
 using Statistics
 
-function _predict(;reference::DataFrame, data_predict::DataFrame, model, predict_function, n_features::Integer, precision::Union{Integer, Nothing})
+function _predict(;reference::DataFrame, data_predict::DataFrame, model, predict_function, n_features::Integer,
+                  n_target_features::Integer, n_instances_explain::Integer, sample_size::Integer, precision::Union{Integer, Nothing})
 
   data_model = data_predict[:, 1:n_features]
   data_meta = data_predict[:, (n_features + 1):size(data_predict, 2)]
@@ -24,13 +25,37 @@ function _predict(;reference::DataFrame, data_predict::DataFrame, model, predict
   data_predicted.shap_effect = data_predicted.real_target - data_predicted.fake_target
   #----------------------------------------------------------------------------
   DataFrames.select!(data_predicted, [:index, :sample, :feature_name, :shap_effect])
-
+  #----------------------------------------------------------------------------
   # Final Shapley value calculation collapsed across Monte Carlo samples.
-  data_predicted = DataFrames.by(data_predicted, [:index, :feature_name],
-                                 shap_effect_sd = :shap_effect => x -> Statistics.std(x),
-                                 shap_effect = :shap_effect => x -> Statistics.mean(x),
-                                 )
 
+  # data_predicted = DataFrames.by(data_predicted, [:index, :feature_name],
+  #                                shap_effect_sd = :shap_effect => x -> Statistics.std(x),
+  #                                shap_effect = :shap_effect => x -> Statistics.mean(x),
+  #                                )
+  
+  feature_name = repeat(data_predicted.feature_name[1:n_target_features], outer = n_instances_explain)
+  index = repeat(1:n_instances_explain, inner = n_target_features)
+
+  indices = map(i -> i * (n_target_features * sample_size), vcat([0], collect(1:(n_instances_explain - 1))))
+  indices_list = Array{Any}(undef, n_instances_explain)
+  for i in 1:n_instances_explain
+     indices_list[i] = map(j -> (j + indices[i]):n_target_features:((n_target_features * sample_size + j - 1) + indices[i]), 1:n_target_features)
+  end
+
+  shap_effect_instance_feature = [Array{Any}(undef, n_target_features) for i in 1:n_instances_explain]
+  shap_effect_instances = Array{Any}(undef, n_instances_explain)
+  for i in 1:n_instances_explain
+    for j in 1:n_target_features
+      shap_effect_instance_feature[i][j] = reshape([Statistics.mean(data_predicted[indices_list[i][j], :shap_effect]), Statistics.std(data_predicted[indices_list[i][j], :shap_effect])], 1, 2)
+    end
+    shap_effect_instances[i] = vcat(shap_effect_instance_feature[i]...)
+  end
+
+  data_predicted = DataFrames.DataFrame(vcat(shap_effect_instances...))
+  rename!(data_predicted, [:shap_effect, :shap_effect_sd])
+  data_predicted.index = index
+  data_predicted.feature_name = feature_name
+  #----------------------------------------------------------------------------
   data_predicted.intercept = repeat([intercept], size(data_predicted, 1))
 
   # Optional rounding of Shapley results to reduce dataset size.
